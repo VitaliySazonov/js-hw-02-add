7f6ce1bd47a163f762ab5870d7b82a40588f552e
Vito <topvetal@gmail.com>
2021-01-15 18:22:04 +0200

    task-07

--- task-07.js
+++ task-07.js
@@ -1,5 +1,16 @@
 //Написати ф-ю яка приймає масив і видаляє звідти всі унікальні елементи
 
+let nonUniqueElements = (arr) => {
+  let
+    data = [],
+    result = []
+  for (let el of arr) {
+    if (!data[el]) data[el] = 0
+    data[el]++
+  }
+  for (let el of arr) if (data[el] > 1) result.push(el)
+  console.log(result)
+}
 
 
 nonUniqueElements([1, 2, 3, 1, 3]) // [1, 3, 1, 3]
